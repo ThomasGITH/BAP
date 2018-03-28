94774e2f7cb6e2a4eca579587ea0844ef5cbab29
ThomasGITH <31830553+ThomasGITH@users.noreply.github.com>
2018-03-28 11:42:57 +0200

    Add files via upload

---
+++ Fouten_opdracht/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Thomas Linssen GD1B</h3>
+<hr>
+<pre>
+  <form method="POST" action="index.php">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="e-mail">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
